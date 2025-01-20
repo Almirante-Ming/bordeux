795ed3ab0c89a48bff6ef7a2deb1041c4d6bc2d6
V. Guilherme <v.guilherme.barreto@gmail.com>
2025-01-20 16:15:13 -0400

    adicionado react e primeira migracao

---
+++ migrations/versions/b0e186e8d52d_first.py
@@ -0,0 +1,41 @@
+"""first
+
+Revision ID: b0e186e8d52d
+Revises: 
+Create Date: 2025-01-17 07:45:38.253410
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b0e186e8d52d'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'authors',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(50), nullable=False),
+        sa.Column('email', sa.String(50), nullable=False),
+        sa.Column('nickname', sa.String(50), nullable=False),
+        sa.Column('active', sa.Boolean, default=True)
+    )
+    op.create_table(
+        'posts',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('title', sa.String(50), nullable=False),
+        sa.Column('content', sa.String(50), nullable=False),
+        sa.Column('author_id', sa.Integer, sa.ForeignKey('authors.id'), nullable=False),
+        sa.Column('date', sa.DateTime, nullable=False),
+        sa.column('priority', sa.String(50), nullable=False)
+    )
+def downgrade() -> None:
+    op.drop_table('authors')
+    op.drop_table('posts')
